283d3dfa85b669dd98f81c179f1b9fb12b0e2a6f
scottamass <78789252+scottamass@users.noreply.github.com>
2021-08-31 13:12:45 +0100

    initial version
    basic code

---
+++ v1.0/main.py
@@ -0,0 +1,23 @@
+#pomodoro time
+#v1.0
+#scott atkinson
+
+import time
+from plyer import notification
+
+count = 0
+print("The pomodoro timer has started, start working!")
+
+if __name__ == "__main__":
+    while True:
+        time.sleep(1800)
+        count += 1
+        notification.notify(
+            title = "Good work!",
+            message = "Take a 10 minute break! You have completed " + str(count) + " pomodoros so far",
+        )
+        time.sleep(600)
+        notification.notify(
+            title = "Back to work!",
+            message = "Try doing another pomodoro...",
+        )
